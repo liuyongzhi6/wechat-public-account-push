08b6b27fe84bf4abde1ad796c8e129419ef28ae5
liuyongzhi6 <114241005+liuyongzhi6@users.noreply.github.com>
2022-09-23 22:30:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oQpxU6C63HTuYIhxsVrwropBH4XI",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "xQBcC2i2Ssz3ldgL7PhZDbfySz0an8Y1NsIDhNUiJgY",
       // 所在省份
       province: "广东",
       // 所在城市
